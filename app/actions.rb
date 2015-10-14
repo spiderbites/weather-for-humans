12d16e86744e0fdbf49706491d86155915fb9648
spiderbites <zuckerje@gmail.com>
2015-10-14 20:35:33 +0000

    basic functionality to get user location from browser geolocation: - added route that takes latitude and longitude query parameters - api testing in open_weather_testing

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,9 @@
 get '/' do
   erb :index
 end
+
+get '/location' do
+  @latitude = params[:lat]
+  @longitude = params[:long]
+  erb :location
+end 
---
+++ open_weather_testing/forecast_io_test.rb
@@ -0,0 +1,5 @@
+require 'forecast_io'
+
+ForecastIO.configure do |configuration|
+  configuration.api_key = '97bab1497ba6cbe99a3add523bdab865'
+end
