9c2c2a6ed9c05d5c3232a35983c652fcf8707dca
Ajwah <johnson786.kevin+git@gmail.com>
2015-10-14 23:21:26 +0000

    Refactor to manage weather helper functions by a class.

--- app/actions/locations.rb
+++ app/actions/locations.rb
@@ -1,34 +1,45 @@
+class Weather
+  OPEN_WEATHER_OPTIONS = { units: "metric", APPID: "66cd0a1a0f9e1272ae428b4f5a9a1e9c" }
+
+  attr_reader :lat, :long, :city_country
+
+  def initialize(config)
+    @lat = config[:lat]
+    @long = config[:long]
+    @city_country = config[:city_country]
+  end
+
+  def get_current_weather_geocode
+    OpenWeather::Current.geocode(lat, long, OPEN_WEATHER_OPTIONS)
+  end
+
+  def get_current_weather_city_country
+    # for now assuming user input is valid, should error check, or better yet error check on front page
+    OpenWeather::Current.city(city_country, OPEN_WEATHER_OPTIONS)
+  end
+
+end
+
 require 'open_weather'
 
 get '/location' do
   if params[:lat] && params[:long]
-    @current_weather = get_current_weather_geocode(params[:lat], params[:long])
+    @weather = Weather.new(params)
+    @current_weather = @weather.get_current_weather_geocode
   end
 
   erb :location
-end 
+end
 
 post '/location' do
   if params[:city_country]
-    @current_weather = get_current_weather_city_country(params[:city_country])
+    @weather = Weather.new(params)
+    @current_weather = @weather.get_current_weather_city_country
   end
 
   erb :location
 end
 
 helpers do
-
-  def open_weather_options
-    { units: "metric", APPID: "66cd0a1a0f9e1272ae428b4f5a9a1e9c" }
-  end
-
-  def get_current_weather_geocode(lat, long)
-    OpenWeather::Current.geocode(lat, long, open_weather_options)
-  end
-
-  def get_current_weather_city_country(city_country)
-    # for now assuming user input is valid, should error check, or better yet error check on front page
-    OpenWeather::Current.city(city_country, open_weather_options)
-  end
 
 end
