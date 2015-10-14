ecd56ca0d71133a386fa99c699ec4c231fea6335
Ajwah <johnson786.kevin+git@gmail.com>
2015-10-14 23:17:58 +0000

    Implement class method that given body part, user specifics and weather obj returns an array of corresponding wearables objects.

--- app/models/wearables.rb
+++ app/models/wearables.rb
@@ -3,4 +3,7 @@ class Wearables < ActiveRecord::Base
     Wearables.distinct.select(:body_part).map(&:body_part)
   end
 
+  def self.get_appropriate_clothing(body_part, user_specifications, weather_data)
+    Wearables.where("gender = #{user_specifications[:gender]} and body_part = #{body_part} and temp_min <= #{weather_data.temperatue} and temp_max >= #{weather_data.temperature}")
+  end
 end
