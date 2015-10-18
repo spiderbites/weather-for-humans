fae330d17004ba2254dd3beca9073eb342512926
spiderbites <zuckerje@gmail.com>
2015-10-18 02:42:33 +0000

    what you'll need later feature, adding all files now

--- app/actions/locations.rb
+++ app/actions/locations.rb
@@ -1,16 +1,16 @@
 require 'json'
 
 helpers do
-  def get_clothing
+  def get_clothing(weather)
     body_parts = Wearable.get_body_parts
 
     clothes = body_parts.reduce({}) do |a, e|
-      appropriate_clothes = Wearable.get_appropriate_clothing(e, { gender: "M" }, @weather)
+      appropriate_clothes = Wearable.get_appropriate_clothing(e, { gender: "M" }, weather)
       a[e] = appropriate_clothes unless appropriate_clothes.empty?
       a
     end
     extras = body_parts.reduce({}) do |a,e|
-      appropriate_extras = Wearable.need_extras(e, @weather.condition)
+      appropriate_extras = Wearable.need_extras(e, weather.condition)
       a[e] = appropriate_extras unless appropriate_extras.empty?
       a
     end
@@ -19,18 +19,24 @@ helpers do
 end
 
 get '/location/:city_country' do
-  @forecast = Forecast.new(params, 0) # only get current temp for now
+  @forecast = Forecast.new(params, 1)
+  
   @weather = @forecast.forecast[0] #Weather.new(params)
-  @clothes = get_clothing
+  @all_weather = @forecast.forecast
+
+  @clothes = get_clothing(@weather)
+  @all_clothes = Wearable.prune(@all_weather.map{ |weather| get_clothing(weather) })
   erb :location
 end
 
-get '/location' do
-  @forecast = Forecast.new(params, 0) # only get current temp for now
-  @weather = @forecast.forecast[0] #Weather.new(params)
-  @clothes = get_clothing
-  erb :location
-end
+# get '/location' do
+#   @forecast = Forecast.new(params, 1) # only get current temp for now
+#   @weather = @forecast.forecast[0] #Weather.new(params)
+#   @future_weather = @forecast.forecast[1]
+#   @clothes = get_clothing(@weather)
+#   @future_clothes = get_clothing(@future_weather)
+#   erb :location
+# end
 
 post '/location' do
   redirect "location/#{params[:city_country]}"
