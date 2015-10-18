781fd80b19811c6b24a11a7ab5b09c0656573cf4
spiderbites <zuckerje@gmail.com>
2015-10-18 00:35:15 +0000

    removing extraneous code

--- app/actions/locations.rb
+++ app/actions/locations.rb
@@ -16,14 +16,6 @@ helpers do
     end
     all_items = clothes.merge(extras)
   end
-
-  def generator(time, temperature)
-      {
-        time: time,
-        temperature: temperature,
-        clothes: yield
-      }
-  end
 end
 
 get '/location/:city_country' do
@@ -40,44 +32,4 @@ end
 
 post '/location' do
   redirect "location/#{params[:city_country]}"
-end
-
-
-
-get '/data' do
-  content_type :json
-  days = %w[Monday Tuesday Wednesday]
-  times = [8, 11, 14, 17, 20, 23]
-  temperatures = [10, 13, 12, 15, 17, 24, 9, 30]
-  all_clothes = Wearable.distinct.select(:clothing).map(&:clothing)
-
-  cycles = days.map do |day|
-
-    time_temp = times.map do |time|
-      [time, temperatures.sample]
-    end
-
-    cycle = time_temp.map do |time, temperature|
-      generator(time, temperature) { all_clothes.sample(5) }
-    end
-
-    {
-      day: day,
-      cycle: cycle
-    }
-  end
-  cycles.to_json
-end
-
-get '/experiment_kevin' do
-  @temperature = { today: [1,2,3,4], tomorrow: [1,5,7,8] }.to_json
-
-  erb :kevin
-end
-
-get '/experiment_robynn' do
-  @weather = Weather.new({city_country: 'Bogota'})
-  # puts Wearable.need_extras(@weather.condition).inspect
-  # puts get_extras
-  ""
 end
