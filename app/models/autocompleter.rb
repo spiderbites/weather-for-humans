89ff108b6f2bd738595691ac325ad0a76d3d32df
spiderbites <zuckerje@gmail.com>
2015-10-21 18:37:59 +0000

    Autocompleter class to make requests to geobytes

---
+++ app/models/autocompleter.rb
@@ -0,0 +1,12 @@
+require 'net/http'
+
+class Autocompleter
+
+  GEOBYTES_URI = "http://gd.geobytes.com/AutoCompleteCity?callback=?&q="
+
+  def self.complete(q)
+    uri = URI(GEOBYTES_URI + q)
+    Net::HTTP.get(uri) # => String
+  end
+
+end
